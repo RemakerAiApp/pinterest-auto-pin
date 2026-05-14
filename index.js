c71a438486bec40bf079bb936888844f96c21eb8
Amar Sanga <hindigyrotech@gmail.com>
2026-05-14 21:07:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,65 +1,31 @@
 const axios = require("axios");
 
-      const src = $(el).attr("src");
+async function run() {
 
-      if (src) {
-        images.push(src);
-      }
-    });
+  console.log("Pinterest Auto Pin Bot Started");
 
-    for (let i = 0; i < images.length; i++) {
+  try {
 
-      const imageUrl = images[i];
+    const response = await axios.get(
+      "https://prompt.image-extractor.com/wp-json/wp/v2/posts?per_page=5"
+    );
 
-      const uniqueId = `${postId}-${i}`;
+    const posts = response.data;
 
-      if (postedPins.includes(uniqueId)) {
-        console.log("Already Posted:", uniqueId);
-        continue;
-      }
+    console.log("Posts Found:", posts.length);
 
-      try {
+    for (const post of posts) {
 
-        const originalPath = path.join(TEMP_DIR, `original-${uniqueId}.jpg`);
-        const finalPath = path.join(TEMP_DIR, `final-${uniqueId}.jpg`);
+      console.log("Post Title:", post.title.rendered);
 
-        console.log("Downloading Image...");
-
-        await downloadImage(imageUrl, originalPath);
-
-        console.log("Adding Watermark...");
-
-        await addWatermark(originalPath, finalPath);
-
-        const aiTitle = generateTitle(postTitle, i);
-
-        const aiDescription = generateDescription(aiTitle);
-
-        console.log("Uploading Pinterest Pin...");
-
-        await createPin(
-          page,
-          finalPath,
-          aiTitle,
-          aiDescription,
-          postLink
-        );
-
-        console.log("Pin Uploaded:", aiTitle);
-
-        postedPins.push(uniqueId);
-
-        await savePostedPins(postedPins);
-
-      } catch (err) {
-
-        console.log("Error:");
-        console.log(err.message);
-      }
     }
+
+  } catch (err) {
+
+    console.log("Error:", err.message);
+
   }
 
-  await browser.close();
 }
 
-processPosts();
+run();
